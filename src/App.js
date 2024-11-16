19b0295a36d9dd146c76b056bcbf59507ff8ce19
Mus1i <v_kazin@mail.ru>
2024-11-16 16:36:34 +0300

    add component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { MyComponent } from './MyComponent';
 
 function App() {
 	return (
@@ -17,6 +18,7 @@ function App() {
 				>
 					Learn React
 				</a>
+				<MyComponent type="normal" value="123" />
 			</header>
 		</div>
 	);
